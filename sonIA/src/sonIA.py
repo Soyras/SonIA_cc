5b00573d8fe33a63058b9afcd53b17923ad548dc
Soyras <74102984+Soyras@users.noreply.github.com>
2020-12-02 19:34:59 +0100

    test

--- sonIA/src/sonIA.py
+++ sonIA/src/sonIA.py
@@ -4,4 +4,8 @@ class Bot(object):
         pass
 
     def act(self, xdif, ydif, vel):
-      	pass
+        if (ydif < 10 or xdif < 10):
+            return 1
+        if xdif < 15 and ydif > 10:
+            return 0
+        pass
